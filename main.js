0cadb4f529e611d0ad9a49391e32448eed09b8f4
Isac Ljungberg <isac.ljungberg3@gmail.com>
2024-04-01 19:26:54 +0200

    update

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ if(document.getElementById("WIP") != null){
 //"macros"
 if(document.getElementById("listOfGods") != null){
     document.getElementById("listOfGods").innerHTML = "<ul>" +
-        "<li><a href=\"bane.html\">Bane</a></li>" +
-        "<li><a href=\"bhaal.html\">Bhaal</a></li>" +
-        "<li><a href=\"lathander.html\">Lathander</a></li>" +
-        "<li><a href=\"necrigal.html\">Necrigal</a></li>" +
-        "<li><a href=\"selune.html\">Sel\u{FB}ne</a></li>" +
+        "<li>[[Bane]]</li>" +
+        "<li>[[Bhaal]]</li>" +
+        "<li>[[Lathander]]</li>" +
+        "<li>[[Necrigal]]</li>" +
+        "<li>[[Selûne]]</li>" +
     "</ul>";
 }
 
@@ -40,9 +40,43 @@ function absoluteDirectoryStart() {
 
 //contains a list of all the links obsidian style links we want to replace with their proper link counterparts, and replaces them all
 function replaceObsiLinks(string){
+    string = string.replaceAll("[[Soul Coin]]", "<a href = \"" + absoluteDirectory("boons/soulCoin.html") + "\">Soul Coin</a>");
+        string = string.replaceAll("[[Soul Coins]]", "<a href = \"" + absoluteDirectory("boons/soulCoin.html") + "\">Soul Coins</a>");
+
+    string = string.replaceAll("[[Aberration]]", "<a href = \"" + absoluteDirectory("aberration.html") + "\">Aberration</a>");
+        string = string.replaceAll("[[Aberrations]]", "<a href = \"" + absoluteDirectory("aberration.html") + "\">Aberrations</a>");
+
+    string = string.replaceAll("[[Bane]]", "<a href = \"" + absoluteDirectory("bane.html") + "\">Bane</a>");
+
+    string = string.replaceAll("[[Bhaal]]", "<a href = \"" + absoluteDirectory("bhaal.html") + "\">Bhaal</a>");
+
+    string = string.replaceAll("[[Ethereal Plane]]", "<div id = \"deadLink\"><a href = \"" + absoluteDirectory("etherealPlane.html") + "\">Ethereal Plane</a></div>");
+
+    string = string.replaceAll("[[Expected Adventurer Income]]", "<a href = \"" + absoluteDirectory("expectedAdventurerIncome.html") + "\">Expected Adventurer Income</a>");
+
+    string = string.replaceAll("[[Gods]]", "<a href = \"" + absoluteDirectory("gods.html") + "\">Gods</a>");
+        string = string.replaceAll("[[God]]", "<a href = \"" + absoluteDirectory("gods.html") + "\">God</a>");
+
+    string = string.replaceAll("[[Lathander]]", "<a href = \"" + absoluteDirectory("lathander.html") + "\">Lathander</a>");
+
+    string = string.replaceAll("[[Loot Tables]]", "<a href = \"" + absoluteDirectory("lootTables.html") + "\">Loot Tables</a>");
+
+    string = string.replaceAll("[[Necrigal]]", "<a href = \"" + absoluteDirectory("necrigal.html") + "\">Necrigal</a>");
+
+    string = string.replaceAll("[[Outer Planes]]", "<div id = \"deadLink\"><a href = \"" + absoluteDirectory("outerPlanes.html") + "\">Outer Planes</a></div>");
+
+    string = string.replaceAll("[[Prime Material]]", "<a href = \"" + absoluteDirectory("primeMaterial.html") + "\">Prime Material</a>");
+        string = string.replaceAll("[[Prime Material Plane]]", "<a href = \"" + absoluteDirectory("primeMaterial.html") + "\">Prime Material Plane</a>");
+
+    string = string.replaceAll("[[Sel\u{FB}ne]]", "<a href = \"" + absoluteDirectory("selune.html") + "\">Sel\u{FB}ne</a>");
+
+    string = string.replaceAll("[[Soulflesh]]", "<a href = \"" + absoluteDirectory("soulflesh.html") + "\">Soulflesh</a>");
+
     string = string.replaceAll("[[Souls]]", "<a href = \"" + absoluteDirectory("souls.html") + "\">Souls</a>");
         string = string.replaceAll("[[Soul]]", "<a href = \"" + absoluteDirectory("souls.html") + "\">Soul</a>");
 
-    string = string.replaceAll("[[Soul Coin]]", "<a href = \"" + absoluteDirectory("boons/soulCoin.html") + "\">Soul Coin</a>");
+    string = string.replaceAll("[[Nine Hells]]", "<a href = \"" + absoluteDirectory("nineHells.html") + "\">Nine Hells</a>");
+
+    string = string.replaceAll("[[Wealth Classes]]", "<a href = \"" + absoluteDirectory("wealthClasses.html") + "\">Wealth Classes</a>");
     return string;
 }
