7b0d32a676a646a2c56c17c6b2ac0aea3986aa27
Isac Ljungberg <isac.ljungberg3@gmail.com>
2024-04-01 18:45:27 +0200

    update

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 if(document.getElementById("indexlink") != null){
-    document.getElementById("indexlink").innerHTML = "<a href=\"index.html\">Realms of Aster</a>";
+    //makes sure that those in the main folder and one folder down link to the the right place
+    document.getElementById("indexlink").innerHTML = "<a href=\"" + absoluteDirectory("index.html") + "\">Realms of Aster</a>";
 }
 
 if(document.getElementById("WIP") != null){
@@ -10,10 +11,27 @@ if(document.getElementById("WIP") != null){
 //"macros"
 if(document.getElementById("listOfGods") != null){
     document.getElementById("listOfGods").innerHTML = "<ul>" +
-        "<li><a href=\"selune.html\">Sel\u{FB}ne</a></li>" +
-        "<li><a href=\"necrigal.html\">Necrigal</a></li>" +
         "<li><a href=\"bane.html\">Bane</a></li>" +
         "<li><a href=\"bhaal.html\">Bhaal</a></li>" +
         "<li><a href=\"lathander.html\">Lathander</a></li>" +
+        "<li><a href=\"necrigal.html\">Necrigal</a></li>" +
+        "<li><a href=\"selune.html\">Sel\u{FB}ne</a></li>" +
     "</ul>";
+}
+
+//replacements, i.e. the link list
+let text = document.body.innerHTML;
+
+document.body.innerHTML = text;
+
+console.log(absoluteDirectoryStart());
+
+//appends the given string to the absolute directory start, i.e. takes a relative link and makes it absolute for the whole folder
+function absoluteDirectory(string){
+    return absoluteDirectoryStart() + string;
+}
+
+//gets the start of the absolute directory
+function absoluteDirectoryStart() {
+    return window.location.href.split("asterdnd")[0] + "asterdnd/";
 }
